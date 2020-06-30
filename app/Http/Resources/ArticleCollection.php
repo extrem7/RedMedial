5fdab39aea5522ead95646fbe6f3886a501ae467
Yurii Kinakh <extrem7@meta.ua>
2020-06-30 23:00:54 +0300

    Fix: article collection with pagination

--- app/Http/Resources/ArticleCollection.php
+++ app/Http/Resources/ArticleCollection.php
@@ -2,21 +2,27 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ArticleCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      * @return array
      */
     public function toArray($request)
     {
-        return [
+
+        $array = [
             'data' => $this->collection,
-            'lastPage' => $this->lastPage()
         ];
+        if ($this->resource instanceof LengthAwarePaginator) {
+            $array['lastPage'] = $this->resource->lastPage();
+        }
+        return $array;
     }
 }
