704638a316e66fbc96b05507f087b4095eacca79
Yurii Kinakh <extrem7@meta.ua>
2020-08-26 21:11:02 +0300

    Fix: home excerpt big card

--- Modules/Frontend/Resources/views/articles/includes/card-big.blade.php
+++ Modules/Frontend/Resources/views/articles/includes/card-big.blade.php
@@ -6,7 +6,7 @@
         </a>
         <div class="main-article-card height-mod-3">
             <a href="{{$article->link}}" class="title title-cyan line-cap">{{$article->title}}</a>
-            <div class="article-description line-cap">{!!$article->excerpt!!}</div>
+            <div class="article-description line-cap">{{strip_tags($article->excerpt)}}</div>
             <div class="d-flex justify-content-between">
                 <a href="{{$article->link}}" class="read-all">Read more</a>
                 <div class="article-date">{{$article->date}}
